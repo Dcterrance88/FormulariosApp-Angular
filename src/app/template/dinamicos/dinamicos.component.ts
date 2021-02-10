2d284a97e79c4b986b7fdb3afbd99348110a6d94
Dcterrance88 <>
2021-02-10 16:04:53 -0500

    Añadiendo y eliminando elementos de forma dinamica
    se creo una funcion para agregar y eliminar elementos los cuales afectan
    a un objeto literal ya creado. Dichas funciones se añadieron tanto al
    boton de agregar como al de eliminar.

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -18,16 +18,31 @@ interface Favorito {
 })
 export class DinamicosComponent {
 
+  public nuevoJuego: string = '';
+
   public persona = {
     nombre: 'Terrance',
     favoritos: [
       { id: 1, nombre: 'Doom' },
       { id: 2, nombre: 'NFS-Pro-Street' },
     ]
-
   }
 
-  public guardar(){
+  public agregarJuego(){
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push({ ...nuevoFavorito })
+    this.nuevoJuego = '';
+  }
+
+  public eliminar(index : number) : void {
+    this.persona.favoritos.splice(index, 1);
+  }
+
+  public guardar() : void {
     console.log('Formulario posteado')
   }
 
