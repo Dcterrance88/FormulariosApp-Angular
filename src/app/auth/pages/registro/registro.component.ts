5b7864fea0bac3018bf7d89c647c744513d31583
Dcterrance88 <>
2021-02-11 16:39:00 -0500

    Validaciones personalizadas
    se creo una validacion personalizada para el campo del email el cual
    evalua si los datos ingresados es igual o no a strider, de serlo se
    lanza un error.

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -11,14 +11,33 @@ export class RegistroComponent implements OnInit {
 
   //TODO:Temporal
   public nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  public emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  public noPuedeSerStrider( control: FormControl) {
+    const valor:string = control.value?.trim().toLowerCase();
+    if( valor === 'strider' ){
+      return {
+        noStrider: true
+      }
+    }
+
+    return null; //Cuando una validacion regresa un null significa que todo está bien
+  }
 
   public miFormulario : FormGroup = this._fb.group({
-    nombre:['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]]
+    nombre:['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]],
+    email:['', [Validators.required, Validators.pattern(this.emailPattern)]],
+    username:['', [Validators.required, this.noPuedeSerStrider] ],
   })
 
   constructor(private _fb : FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Terrance Asimov',
+      email: 'terranceasimov@gmail.com',
+      username: 'sepran'
+    })
   }
 
   public campoNoValido(campo : string) {
