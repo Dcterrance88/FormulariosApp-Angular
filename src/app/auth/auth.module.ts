227468b02f36f2dabde6cd393c4b1cace3030663
Dcterrance88 <>
2021-02-11 15:54:41 -0500

    Validando con una expresion regular
    se añadio en el campo nombre una validacion de una expresion regular la
    cual permite evaluar dos string separados por un espacio.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RegistroComponent } from './pages/registro/registro.component';
+
 import { LoginComponent } from './pages/login/login.component';
 
 
 @NgModule({
   declarations: [
     RegistroComponent,
-    LoginComponent],
+    LoginComponent
+  ],
   imports: [
+    AuthRoutingModule,
     CommonModule,
-    AuthRoutingModule
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -8,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  //TODO:Temporal
+  public nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+
+  public miFormulario : FormGroup = this._fb.group({
+    nombre:['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]]
+  })
+
+  constructor(private _fb : FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  public campoNoValido(campo : string) {
+    return this.miFormulario.get(campo)?.invalid
+            && this.miFormulario.get(campo)?.touched;
+  }
+
+  public submitFormulario(){
+    console.log(this.miFormulario.value);
+    this.miFormulario.markAllAsTouched();
   }
 
 }
