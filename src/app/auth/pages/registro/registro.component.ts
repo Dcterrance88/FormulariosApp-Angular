064631c55638b39ecc629bf8d37147a8cf1772e2
Dcterrance88 <>
2021-02-11 20:07:05 -0500

    Separando la logica de validaciones del componente
    Se separo la logica de validaciones de dos formas, una por medio de un
    archivo ts exportando los patrones y la funcion y la otra por medio de
    un servicio.

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { emailPattern, nombreApellidoPattern, noPuedeSerStrider } from 'src/app/shared/validator/validaciones';
+import { ValidatorService } from 'src/app/shared/validator/validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -9,33 +11,19 @@ import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
 })
 export class RegistroComponent implements OnInit {
 
-  //TODO:Temporal
-  public nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
-  public emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
-
-  public noPuedeSerStrider( control: FormControl) {
-    const valor:string = control.value?.trim().toLowerCase();
-    if( valor === 'strider' ){
-      return {
-        noStrider: true
-      }
-    }
-
-    return null; //Cuando una validacion regresa un null significa que todo está bien
-  }
-
   public miFormulario : FormGroup = this._fb.group({
-    nombre:['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]],
-    email:['', [Validators.required, Validators.pattern(this.emailPattern)]],
-    username:['', [Validators.required, this.noPuedeSerStrider] ],
+    nombre  :['', [Validators.required, Validators.pattern(this._vs.nombreApellidoPattern)]],
+    email   :['', [Validators.required, Validators.pattern(this._vs.emailPattern)]],
+    username:['', [Validators.required, this._vs.noPuedeSerStrider] ],
   })
 
-  constructor(private _fb : FormBuilder) { }
+  constructor(private _fb : FormBuilder,
+              private _vs : ValidatorService) { }
 
   ngOnInit(): void {
     this.miFormulario.reset({
-      nombre: 'Terrance Asimov',
-      email: 'terranceasimov@gmail.com',
+      nombre  : 'Terrance Asimov',
+      email   : 'terranceasimov@gmail.com',
       username: 'sepran'
     })
   }
---
+++ src/app/shared/validator/validator.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { FormControl, ValidationErrors } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidatorService {
+
+  public nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  public emailPattern         : string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  constructor() { }
+
+  public noPuedeSerStrider ( control: FormControl ) : ValidationErrors | null {
+
+    const valor:string = control.value?.trim().toLowerCase();
+    const objValidErr: ValidationErrors = { noStrider: true }
+
+    return (valor === 'strider')? objValidErr : null ;
+  }
+}
